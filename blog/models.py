ae891ec4891a5b6678ddc0558526d021ff9036e8
guff192 <kochkons1@ya.ru>
2022-05-05 21:47:00 +0400

    Created article and comment models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(verbose_name='article title', max_length=255)
+    text = models.TextField(verbose_name='article text', max_length=5000)
+
+
+class Comment(models.Model):
+    article_id = models.ForeignKey('Article', on_delete=models.CASCADE, to_field='id')
+
+    level = models.IntegerField(verbose_name='comment level')
+    parent_path = models.CharField(max_length=255)
+
+    text = models.TextField(verbose_name='comment text', max_length=1000)
+    time_posted = models.DateTimeField(verbose_name='comment posting time', auto_created=True)
+
