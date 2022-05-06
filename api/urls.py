12b08eabcc6a9bf935cd974d42cd2ca3e05fa29c
guff192 <kochkons1@ya.ru>
2022-05-06 19:40:58 +0400

    Changed schema to use less endpoints

--- api/urls.py
+++ api/urls.py
@@ -5,9 +5,7 @@ from api import views
 
 urlpatterns = [
     path('v1/articles/', views.create_article),
-    path('v1/articles/<int:pk>/add_comment', views.add_comment_to_article),
-    path('v1/comments/<int:pk>/reply', views.add_comment_reply),
-    path('v1/articles/<int:pk>/comments', views.get_article_comments),
-    path('v1/comments/<int:pk>/replies', views.get_comment_replies),
+    path('v1/articles/<int:pk>/comments', views.get_article_comments_or_add_new),
+    path('v1/comments/<int:pk>/replies', views.get_comment_replies_or_add_new),
 ]
 
--- api/views.py
+++ api/views.py
@@ -22,60 +22,56 @@ def create_article(request):
     return Response({'message': 'article data is not valid'}, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(http_method_names=['POST'])
-def add_comment_to_article(request, pk):
-    data = JSONParser().parse(request)
-    data['article'] = pk
-    serializer = serializers.CommentCreationSerializer(data=data)
-    if serializer.is_valid():
-       comment_reponse = serializers.CommentSerializer(serializer.save())
-       return Response(comment_reponse.data)
-
-    return Response({'message': 'comment data is invalid'}, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(http_method_names=['POST'])
-def add_comment_reply(request, pk):
-    parent_comment = Comment.objects.get(pk=pk)
-
-    data = JSONParser().parse(request)
-    data['article'] = parent_comment.article.pk
-    data['parent_path'] = f'{parent_comment.parent_path}{parent_comment.pk}/'
-    data['level'] = parent_comment.level + 1
-    
-    serializer = serializers.CommentSerializer(data=data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-
-    return Response({'message': 'reply data is invalid'}, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(http_method_names=['GET'])
-def get_article_comments(request, pk):
+@api_view(http_method_names=['GET', 'POST'])
+def get_article_comments_or_add_new(request, pk):
     try:
         Article.objects.get(pk=pk)
     except Article.DoesNotExist:
         return Response({'message': 'article doesn\'t exist'}, status=status.HTTP_404_NOT_FOUND)
 
-    comments = Comment.objects.filter(article__pk=pk).filter(level__lte=3)\
-            .order_by('parent_path', 'time_posted')
-    serializer = serializers.CommentSerializer(comments, many=True)
+    if request.method == 'GET':
+        comments = Comment.objects.filter(article__pk=pk).filter(level__lte=3)\
+                .order_by('parent_path', 'time_posted')
+        serializer = serializers.CommentSerializer(comments, many=True)
 
-    return Response(serializer.data)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        data['article'] = pk
+        serializer = serializers.CommentCreationSerializer(data=data)
+        if serializer.is_valid():
+           comment_reponse = serializers.CommentSerializer(serializer.save())
+           return Response(comment_reponse.data)
+
+        return Response({'message': 'comment data is invalid'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(http_method_names=['GET'])
-def get_comment_replies(request, pk):
+def get_comment_replies_or_add_new(request, pk):
     try:
         parent_comment = Comment.objects.get(pk=pk)
     except Comment.DoesNotExist:
-        return Response({'message': 'comment doesn\'t exist'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'message': 'article doesn\'t exist'}, status=status.HTTP_404_NOT_FOUND)
 
-    replies_parent_path = f'{parent_comment.parent_path}{parent_comment.pk}/'
-    replies = Comment.objects.filter(parent_path__contains=replies_parent_path)\
-            .order_by('parent_path', 'time_posted')
-    serializer = serializers.CommentSerializer(replies, many=True)
+    if request.method == 'GET':
+        replies_parent_path = f'{parent_comment.parent_path}{parent_comment.pk}/'
+        replies = Comment.objects.filter(parent_path__contains=replies_parent_path)\
+                .order_by('parent_path', 'time_posted')
+        serializer = serializers.CommentSerializer(replies, many=True)
 
-    return Response(serializer.data)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        data['article'] = parent_comment.article.pk
+        data['parent_path'] = f'{parent_comment.parent_path}{parent_comment.pk}/'
+        data['level'] = parent_comment.level + 1
+        
+        serializer = serializers.CommentSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response({'message': 'reply data is invalid'}, status=status.HTTP_400_BAD_REQUEST)
 
